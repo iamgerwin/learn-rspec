e7e33d244bb926c8415d1e674201a7f6dc527f38
John Gerwin De las Alas <iamgerwin@aim.com>
2015-10-08 15:54:35 +0800

    Use context

---
+++ lib/blog.rb
@@ -0,0 +1,14 @@
+class Blog
+
+	def initialize posts: []
+		@posts = posts
+	end
+
+	def empty?
+		@posts.empty?
+	end
+
+	def posts_count
+		@posts.count
+	end
+end
---
+++ spec/blog_spec.rb
@@ -0,0 +1,25 @@
+require 'blog'
+require 'post'
+
+describe Blog do
+
+	context "with no posts" do
+		let(:blog) { Blog.new }
+
+		it "has no posts" do
+			expect(Blog.new).to be_empty
+		end
+	end
+
+	context "with one post" do
+		let(:blog) { Blog.new(posts: [Post.new]) }
+
+		it "has a post when initialized with one" do
+			expect(blog).not_to be_empty
+		end
+
+		it "counts the number of posts" do
+			expect(blog.posts_count).to eq 1
+		end
+	end
+end
